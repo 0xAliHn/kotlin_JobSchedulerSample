32cecb56338b95dfb268f42a656d734c29564801
0xAliHn <ali.ict06@gmail.com>
2018-04-06 00:53:57 +0600

    Initial commit

---
+++ app/src/main/java/com/alihn/android/jobscheduler/Constants.kt
@@ -0,0 +1,12 @@
+@file:JvmName("Constants")
+@file:Suppress("PropertyName")
+
+package com.alihn.android.jobscheduler
+
+@JvmField val MSG_UNCOLOR_START = 0
+@JvmField val MSG_UNCOLOR_STOP = 1
+@JvmField val MSG_COLOR_START = 2
+@JvmField val MSG_COLOR_STOP = 3
+
+@JvmField val MESSENGER_INTENT_KEY = "${BuildConfig.APPLICATION_ID}.MESSENGER_INTENT_KEY"
+@JvmField val WORK_DURATION_KEY = "${BuildConfig.APPLICATION_ID}.WORK_DURATION_KEY"
